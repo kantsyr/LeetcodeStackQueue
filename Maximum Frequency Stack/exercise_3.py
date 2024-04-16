4bfe2e8369637e0c712123b12fc22481c57dad61
kantsyr <katia.kantsyr@gmail.com>
2024-04-16 22:29:51 +0300

    Correct implementation of Implement Maximum Frequency Stack task(using defaultdict)

---
+++ Maximum Frequency Stack/exercise_3.py
@@ -0,0 +1,29 @@
+from collections import defaultdict, deque
+
+class FreqStack(object):
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(deque)
+        self.max_f = 0 #max frequency 
+
+    def push(self, value):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.group[self.freq[value] + 1].append(value)
+        self.freq[value] += 1
+        self.max_f = max(self.max_f, self.freq[value])
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        value = self.group[self.max_f].pop()
+        self.freq[value] -= 1
+
+        if not self.group[self.max_f]:
+            self.max_f -= 1
+
+        return value
