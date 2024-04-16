a630db0b3f53270467bf382e5bd188b48e25a72b
kantsyr <katia.kantsyr@gmail.com>
2024-04-16 20:16:38 +0300

    Correct implementation of Implement Queue using Stack task with all requirements

--- Implement Queue using Stacks/exercise_1.py
+++ Implement Queue using Stacks/exercise_1.py
@@ -3,53 +3,83 @@ class Node:
         self.data = data
         self.next = None
 
+class Stack:
+    def __init__(self):
+        self.head = Node(0)
+        self.size = 0
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def push(self, value):
+        node = Node(value)
+        self.size += 1
+
+        node.next = self.head
+        self.head = node
+
+    def pop(self):
+        self.size -= 1
+        deleted = self.head
+        self.head = self.head.next
+
+        return deleted.data
+    
+    def peek(self):
+        return self.head.data
+    
 class MyQueue(object):
     def __init__(self):
-        self.head = None
+        self.queue = Stack()
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        if self.head is None:
-            self.head = Node(x)
-        else:
-            new_n = Node(x)
-            new_n.next = self.head
-            self.head = new_n
+        self.queue.push(x)
 
     def pop(self):
         """
         :rtype: int
         """
-        current = self.head
-        previous = None
+        new_q_1 = Stack()
+        new_q_2 = Stack()
 
-        while current.next is not None:
-            previous = current
-            current = current.next
+        while not self.queue.isEmpty():
+            new_q_1.push(self.queue.pop())
 
-        if previous is not None:
-            previous.next = None
-        else:
-            self.head = None
+        result = new_q_1.pop()
 
-        return current.data
+        while not new_q_1.isEmpty():
+            new_q_2.push(new_q_1.pop())
+
+        self.queue = new_q_2
+
+        return result
+
 
     def peek(self):
         """
         :rtype: int
         """
-        current = self.head
+        new_q_1 = Stack()
+        new_q_2 = Stack()
 
-        while current.next is not None:
-            current = current.next
+        while not self.queue.isEmpty():
+            new_q_1.push(self.queue.pop())
 
-        return current.data
+        result = new_q_1.peek()
+
+        while not new_q_1.isEmpty():
+            new_q_2.push(new_q_1.pop())
+
+        self.queue = new_q_2
+
+        return result
 
     def empty(self):
         """
         :rtype: bool
         """
-        return self.head is None
+        return self.queue.isEmpty()
